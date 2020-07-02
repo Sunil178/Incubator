fadd53cf6c2e165d9856f7cb81904d1fd5329c8c
Pratik <icanpratikpawar@gmail.com>
2020-07-02 19:28:42 +0530

    changes

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -439,7 +439,7 @@ class AdminController extends Controller
         $insta_break=explode(" ",$new_str);
         $instagram['followers']=$insta_break[0];
         $instagram['following']="59";
-        print_r($insta_break);
+        // print_r($insta_break);
         // dd($insta_break);
 
         $revenue_data = DB::select('SELECT payment_month as label, sum(monthly_rent)/sum(no_of_seats) as value from company_revenues group by payment_month order by payment_month Desc;');
